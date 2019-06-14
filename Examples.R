cf3021b1e924751ba92e320103a9ce2b73ea9ed7
beckyfisher <rebeccafisher76@gmail.com>
2019-06-14 10:22:09 +0800

    Added example of using the R only version and supplying data using the new function supplyData

--- Examples.R
+++ Examples.R
@@ -1,4 +1,6 @@
 
+
+#### - Using excel workbook interface ------------------------------------------
 # insert data into the spreadsheet for each of the region, with each site treated
 # as an "impact", and run the analysis.
 #options(java.parameters = "-Xmx16g" )
@@ -6,21 +8,57 @@
 #require(XLConnect)
 require(epower)
 
-setwd("C:/Users/rfisher/Dropbox/EPower/Toobox_paper_draft1/Examples")
-fitData(excelInFile="Most_complex_simulated.xlsx") # takes a while to run
+#setwd("C:/Users/rfisher/Dropbox/EPower/Toobox_paper_draft1/Examples")
+#fitData(excelInFile="Most_complex_simulated.xlsx") # takes a while to run
 
-setwd("C:/Users/rfisher/Dropbox/EPower/Toobox_paper_draft1/Examples")
+
 fitData(excelInFile="DBCA_fixed_transect_Tantabiddi.xlsx")
 assessPower()
 
-setwd("C:/Users/rfisher/Dropbox/EPower/Toobox_paper_draft1/Examples")
+
 fitData(excelInFile="Simple_gaussian_simulated.xlsx")
 assessPower()
 
 
 
 ### example using only R to interface with the package
-dat<-read.csv("C:/Users/rfisher/Dropbox/EPower/Toobox_paper_draft1/Revision1/DBCA_fixed_transect_Tantabiddi.csv")
+dat<-read.csv("epower_interface_V1.3_pilot_data.csv")
+
+dataComponents<-supplyData(
+	dat=dat,
+  variableType="binomial",
+  design.matrix=list(
+      Response="CORAL",
+      Trials="n.scored",
+      Location="Site",
+      sublocation="Replicate",
+      Time="SurveyID",
+      "subtime"=NA,
+      BvA="BvA",
+      CvI="CvI"),
+  levels.dat=list(
+      Before="Before",
+      Control="control",
+      After="After",
+      Impact="impact"),
+  scenario.data=list(
+      Number.of.iterations=500,
+      filename="test",
+      Number.of.Impact.Locations=1,
+      Number.of.Control.Locations=3,
+      Number.of.sublocations.within.Location="3;4",
+      Number.of.sample.times.Before=3,
+      Number.of.sample.times.After=2,
+      Number.of.subtimes.within.Time=1,
+      Number.of.trials="100;200",
+      Number.of.replicate.measurements=1),
+  effect.info=list(
+      Multiplicative=1,
+      Fixed.change=0,
+      Effect.values=-0.3))
+# now run powerScenario
+require(INLA,quietly=TRUE)
+scenarioParams<-powerScenario(inputData=dataComponents)
 
 
 
