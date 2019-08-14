a1b4e06dc523bda39942f149fb38321e05abd33f
beckyfisher <rebeccafisher76@gmail.com>
2019-08-14 12:51:11 +0800

    Added header info

--- Examples.R
+++ Examples.R
@@ -1,4 +1,20 @@
-
+# epower conducts a power analysis on beyond BACI sampling designs through 
+# INLA based inference and Monte Carlo simulation. Currently epower considers 
+# only univariate responses. The analysis allows for cross random effects between 
+# Time and Site in the design. The epower toolbox has been designed for use by 
+# ecologists with a sound understanding of the statistical principles of BACI designs. 
+# To assist the non-R user, BACI parameters are defined in an excel workbook template.
+# 
+# Here how to use the function with and without the excel interface.
+# Date: 14-Aug-2019
+# Authors: 
+# Rebecca Fisher
+# Glenn R Shiell
+# Rohan J Sadler
+# Karina Inostroza 
+# George Shedrawi
+# Thomas H Holmes
+# James M McGree7
 
 #### - Using excel workbook interface ------------------------------------------
 # insert data into the spreadsheet for each of the region, with each site treated
@@ -12,7 +28,6 @@ require(epower)
 
 setwd("C:/Users/rfisher/OneDrive - Australian Institute of Marine Science/Documents/AIMS/EcologicalRiskModelling/EPower/epower_examples")
 #fitData(excelInFile="Most_complex_simulated.xlsx") # takes a while to run
-
 
 fitData(excelInFile="DBCA_fixed_transect_Tantabiddi.xlsx")
 assessPower()
